01104c6021a3c4379f509862c96d216fb2c716ec
luiz.lima <luiz.lima@compasso.com.br>
2022-04-01 11:25:06 -0300

    Versão 2.0

--- AluraViagens/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ AluraViagens/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -1,8 +1,13 @@
 package br.com.alura.aluraviagens.ui.activity;
 
+import static br.com.alura.aluraviagens.ui.activity.PacoteActivityConstantes.CHAVE_PACOTE;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.List;
@@ -15,6 +20,7 @@ public class ListaPacotesActivity extends AppCompatActivity {
 
     public static final String TITULO_APPBAR = "Pacotes";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +28,27 @@ public class ListaPacotesActivity extends AppCompatActivity {
         setTitle(TITULO_APPBAR);
 
         configuraLista();
+
+
     }
 
     private void configuraLista() {
         ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
         List<Pacote> pacotes = new PacoteDAO().lista();
         listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
+        listaDePacotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+                Pacote pacoteClicado = pacotes.get(posicao);
+                vaiParaResumoPacote(pacoteClicado);
+            }
+
+            private void vaiParaResumoPacote(Pacote pacoteClicado) {
+                Intent intent = new Intent(ListaPacotesActivity.this,
+                        ResumoPacoteActivity.class);
+                intent.putExtra(CHAVE_PACOTE, pacoteClicado);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ AluraViagens/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
@@ -0,0 +1,110 @@
+package br.com.alura.aluraviagens.ui.activity;
+
+import static br.com.alura.aluraviagens.ui.activity.PacoteActivityConstantes.CHAVE_PACOTE;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import br.com.alura.aluraviagens.R;
+import br.com.alura.aluraviagens.model.Pacote;
+import br.com.alura.aluraviagens.util.DiasUtil;
+import br.com.alura.aluraviagens.util.MoedaUtil;
+import br.com.alura.aluraviagens.util.ResourceUtil;
+
+public class ResumoPacoteActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Resumo do Pacote";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(br.com.alura.aluraviagens.R.layout.activity_resumo_pacote);
+
+        setTitle(TITULO_APPBAR);
+
+        carregaPacoteRecebido();
+
+    }
+
+    private void carregaPacoteRecebido() {
+        Intent intent = getIntent();
+        if (intent.hasExtra("pacote")) {
+            final Pacote pacote = (Pacote) intent.getSerializableExtra("pacote");
+
+            inicializaCampos(pacote);
+
+            configuraBotao(pacote);
+
+        }
+    }
+
+    private void configuraBotao(Pacote pacote) {
+        Button botaoRealizaPagamento = findViewById(R.id.resumo_pacote_botao_realiza_pagamento);
+        botaoRealizaPagamento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vaiParaPagamento(pacote);
+            }
+        });
+    }
+
+    private void vaiParaPagamento(Pacote pacote) {
+        Intent intent = new Intent(ResumoPacoteActivity.this,
+                PagamentoActivity.class);
+        intent.putExtra(CHAVE_PACOTE, pacote);
+        startActivity(intent);
+    }
+
+    private void inicializaCampos(Pacote pacote) {
+        motraLocal(pacote);
+        mostraImagem(pacote);
+        mostraDias(pacote);
+        mostraPreco(pacote);
+        mostraData(pacote);
+    }
+
+    private void mostraData(Pacote pacote) {
+        TextView data = findViewById(R.id.resumo_pacote_data);
+        String dataFormatadaDaViagem = DataUtil.periodoEmTexto(pacote.getDias());
+        data.setText(dataFormatadaDaViagem);
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.resumo_pacote_preco);
+        String moedaBrasileira = MoedaUtil
+                .formataParaReal(pacote.getPreco());
+        preco.setText(moedaBrasileira);
+    }
+
+    private void mostraDias(Pacote pacote) {
+        TextView dias = findViewById(R.id.resumo_pacote_dias);
+        String diasEmTexto = DiasUtil.formataEmTexto(pacote.getDias());
+        dias.setText(diasEmTexto);
+    }
+
+    private void mostraImagem(Pacote pacote) {
+        ImageView imagem = findViewById(R.id.resumo_pacote_imagem);
+        Drawable drawableDoPacote = ResourceUtil
+                .devolveDrawable(this, pacote.getImagem());
+        imagem.setImageDrawable(drawableDoPacote);
+    }
+
+    private void motraLocal(Pacote pacote) {
+        TextView local = findViewById(R.id.resumo_pacote_local);
+        local.setText(pacote.getLocal());
+    }
+}
